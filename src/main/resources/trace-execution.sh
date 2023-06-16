4e9af5633338de327b8813c31e11ab2524c0a87d
amir <amirdeljouyi@gmail.com>
2023-06-16 10:02:01 +0200

    update trace-execution.sh

--- src/main/resources/trace-execution.sh
+++ src/main/resources/trace-execution.sh
@@ -4,7 +4,6 @@ echo "$DIR_PATH"
 cd "$DIR_PATH"
 pwd=$(pwd)
 echo "$pwd"
-TRACE_CLASS_PATH=../java/trace/CallTrace.java
 source ./.env
 #OUTPUT_PATH="$pwd/trace-output/$5"
 OUTPUT_PATH=$5
@@ -13,8 +12,8 @@ PACKAGE_NAME=/$2
 echo "START TRACING $PACKAGE_NAME ON PORT $1 and methods=/$3"
 if [ $4 == "false" ]; then
   echo "Output will be written on the file $OUTPUT_PATH"
-  btrace -u -v -d "./dumpClasses" -o "$OUTPUT_PATH" "$1" "$TRACE_CLASS_PATH"  packageName="$PACKAGE_NAME" methodName="/$3"
+  btrace -u -v -d "./dumpClasses" -pd "./pool" -o "$OUTPUT_PATH" "$1" "$6"  packageName="$PACKAGE_NAME" methodName="/$3"
 else
   echo "Output will be shown on Console"
-  btrace -u -v -d "./dumpClasses" "$1" "$TRACE_CLASS_PATH"  packageName="$PACKAGE_NAME" methodName="/$3"
+  btrace -u -v -d "./dumpClasses" -pd "./pool" "$1" "$6"  packageName="$PACKAGE_NAME" methodName="/$3"
 fi
